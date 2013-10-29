16c650e47622242cfb214a0784f3e8822b81d208
Heath Hudgins <whh33@case.edu>
2013-10-29 18:59:41 -0400

    Documents CSS, Permissions etc
    I made the table look a bit more pretty and took out the show
    functionality.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,53 +2,66 @@ class DocumentsController < ApplicationController
   # GET /documents
   # GET /documents.json
   def index
-    @documents = Document.all
+    if !(current_user)
+      redirect_to root_url
+    else
+      @documents = Document.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @documents }
-    end
-  end
-
-  # GET /documents/1
-  # GET /documents/1.json
-  def show
-    @document = Document.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @document }
-    end
+     respond_to do |format|
+       format.html # index.html.erb
+       format.json { render json: @documents }
+     end
+   end
   end
 
   # GET /documents/new
   # GET /documents/new.json
   def new
-    @document = Document.new
+     if !(current_user)
+      redirect_to root_url
+     elsif (current_user && !(current_user.can_edit))
+     else
+      @document = Document.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @document }
+      respond_to do |format|
+       format.html # new.html.erb
+       format.json { render json: @document }
+     end
     end
   end
 
   # GET /documents/1/edit
   def edit
-    @document = Document.find(params[:id])
+    if !(current_user)
+      redirect_to root_url
+     elsif (current_user && !(current_user.can_edit))
+     else
+        if !(current_user)
+          redirect_to root_url
+        elsif (current_user && !(current_user.can_edit))
+        else
+          @document = Document.find(params[:id])
+        end
+    end
   end
 
   # POST /documents
   # POST /documents.json
   def create
-    @document = Document.new(params[:document])
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+    else
+      @document = Document.new(params[:document])
 
-    respond_to do |format|
-      if @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render json: @document, status: :created, location: @document }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @document.save
+          format.html { redirect_to documents_url, notice: 'Document was successfully created.' }
+          format.json { render json: @document, status: :created, location: @document }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @document.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -56,15 +69,20 @@ class DocumentsController < ApplicationController
   # PUT /documents/1
   # PUT /documents/1.json
   def update
-    @document = Document.find(params[:id])
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+    else
+      @document = Document.find(params[:id])
 
-    respond_to do |format|
-      if @document.update_attributes(params[:document])
-        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @document.update_attributes(params[:document])
+          format.html { redirect_to documents_url, notice: 'Document was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @document.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -72,12 +90,17 @@ class DocumentsController < ApplicationController
   # DELETE /documents/1
   # DELETE /documents/1.json
   def destroy
-    @document = Document.find(params[:id])
-    @document.destroy
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+    else
+      @document = Document.find(params[:id])
+      @document.destroy
 
-    respond_to do |format|
-      format.html { redirect_to documents_url }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to documents_url }
+        format.json { head :no_content }
+      end
     end
   end
 end
