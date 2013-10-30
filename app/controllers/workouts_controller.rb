f12ad67e2973751e7905aae06fe6c22da2854c31
David Arnold <Dave@Andonuts.net>
2013-10-29 22:13:03 -0400

    Workouts index styled Permission checks added to workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,53 +2,64 @@ class WorkoutsController < ApplicationController
   # GET /workouts
   # GET /workouts.json
   def index
-    @workouts = Workout.all
+    if !(current_user)
+      redirect_to root_url
+    else
+      @workouts = Workout.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @workouts }
-    end
-  end
-
-  # GET /workouts/1
-  # GET /workouts/1.json
-  def show
-    @workout = Workout.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @workout }
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @workouts }
+      end
     end
   end
 
   # GET /workouts/new
   # GET /workouts/new.json
   def new
-    @workout = Workout.new
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+      redirect_to root_url
+    else
+      @workout = Workout.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @workout }
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @workout }
+      end
     end
   end
 
   # GET /workouts/1/edit
   def edit
-    @workout = Workout.find(params[:id])
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+      redirect_to root_url
+    else
+      @workout = Workout.find(params[:id])
+    end
   end
 
   # POST /workouts
   # POST /workouts.json
   def create
-    @workout = Workout.new(params[:workout])
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+      redirect_to root_url
+    else
+      @workout = Workout.new(params[:workout])
 
-    respond_to do |format|
-      if @workout.save
-        format.html { redirect_to @workout, notice: 'Workout was successfully created.' }
-        format.json { render json: @workout, status: :created, location: @workout }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @workout.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @workout.save
+          format.html { redirect_to @workout, notice: 'Workout was successfully created.' }
+          format.json { render json: @workout, status: :created, location: @workout }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @workout.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -56,15 +67,21 @@ class WorkoutsController < ApplicationController
   # PUT /workouts/1
   # PUT /workouts/1.json
   def update
-    @workout = Workout.find(params[:id])
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+      redirect_to root_url
+    else
+      @workout = Workout.find(params[:id])
 
-    respond_to do |format|
-      if @workout.update_attributes(params[:workout])
-        format.html { redirect_to @workout, notice: 'Workout was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @workout.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @workout.update_attributes(params[:workout])
+          format.html { redirect_to @workout, notice: 'Workout was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @workout.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -72,12 +89,18 @@ class WorkoutsController < ApplicationController
   # DELETE /workouts/1
   # DELETE /workouts/1.json
   def destroy
-    @workout = Workout.find(params[:id])
-    @workout.destroy
+    if !(current_user)
+      redirect_to root_url
+    elsif (current_user && !(current_user.can_edit))
+      redirect_to root_url
+    else
+      @workout = Workout.find(params[:id])
+      @workout.destroy
 
-    respond_to do |format|
-      format.html { redirect_to workouts_url }
-      format.json { head :no_content }
+      respond_to do |format|
+        format.html { redirect_to workouts_url }
+        format.json { head :no_content }
+      end
     end
   end
 end
