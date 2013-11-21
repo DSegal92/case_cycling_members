0707cafb969eb43206d3f034fcf7f64a09f39bd1
David Arnold <Dave@Andonuts.net>
2013-11-21 13:46:43 -0500

    Functional tests for Workouts now pass Login is now “forced” from an admin member fixture before each test so create, edit, delete work

--- test/functional/workouts_controller_crud_test.rb
+++ test/functional/workouts_controller_crud_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class WorkoutsControllerCRUDTest < ActionController::TestCase
 	setup do
 		@controller = WorkoutsController.new
-		# this is a hacky way to force a log-in (it acually isn't; current_user is actually a method)
+		# this is a hacky way to force a log-in
 		@member = members(:admin)
-		@controller.instance_variable_set(:@current_user, @member)
+		# @controller.instance_variable_set(:@current_user, @member) # this is no work
+		session[:member_id] = @member
 		
 		@workout = workouts(:one)
 	end
@@ -15,7 +16,8 @@ class WorkoutsControllerCRUDTest < ActionController::TestCase
 			post :create, workout: { date: @workout.date, description: @workout.description }
 		end
 		
-		assert_response :success
+		#assert_response :success
+		assert_redirected_to workouts_url
 	end
 	
 	test "edit workout" do
@@ -23,8 +25,9 @@ class WorkoutsControllerCRUDTest < ActionController::TestCase
 			put :update, id: @workout, workout: { date: @workout.date, description: @workout.description }
 		#end
 		
-		# why is this 302 instead of 200?
-		assert_response 302
+		# why is this 302 instead of 200? it's a redirect!
+		#assert_response :success
+		assert_redirected_to workouts_url
 	end
 	
 	test "destroy workout" do
@@ -32,6 +35,7 @@ class WorkoutsControllerCRUDTest < ActionController::TestCase
 			delete :destroy, id: @workout
 		end
 		
-		assert_response :success
+		#assert_response :success
+		assert_redirected_to workouts_url
 	end
 end
