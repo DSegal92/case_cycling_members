a125d5cfde73b519a49d64a7e73de00d6c3ef581
David Arnold <Dave@Andonuts.net>
2013-11-21 15:52:54 -0500

    Workout functional test completed Workout functional test comments cleaned up further

--- test/functional/workouts_controller_crud_test.rb
+++ test/functional/workouts_controller_crud_test.rb
@@ -2,15 +2,19 @@ require 'test_helper'
 
 class WorkoutsControllerCRUDTest < ActionController::TestCase
 	setup do
+		# set the controller we're testing since it can't be infferred from the class name
 		@controller = WorkoutsController.new
+		
 		# this is a hacky way to force a log-in
 		@member = members(:admin)
 		session[:member_id] = @member
 		
+		# this is our test object from a fixture
 		@workout = workouts(:one)
 	end
 
 	test "create workout" do
+		# make sure the object was created
 		assert_difference('Workout.count', 1, 'Workout should be added') do
 			post :create, workout: { date: @workout.date, description: @workout.description }
 		end
@@ -20,15 +24,24 @@ class WorkoutsControllerCRUDTest < ActionController::TestCase
 	end
 	
 	test "edit workout" do
-		#assert do
-			put :update, id: @workout, workout: { date: @workout.date, description: @workout.description }
-		#end
+		# generate expected values randomly
+		@date = DateTime.civil_from_format :utc, 2000 + rand(999), rand(12), rand(29), rand(24), rand(60), rand(60)
+		@desc = (0...200).map{ (' '..'~').to_a[rand(94)] }.join
+		
+		# update the existing entry
+		put :update, id: @workout, workout: { date: @date, description: @desc }
 		
 		# we get redirected to the list page upon success, the root on failure
 		assert_redirected_to workouts_url
+		
+		# make sure everything updated as expected
+		@test_workout = Workout.find(@workout)
+		assert_equal(@date, @test_workout.date, "Dates should match")
+		assert_equal(@desc, @test_workout.description, "Descriptions should match")
 	end
 	
 	test "destroy workout" do
+		# make sure the object was removed
 		assert_difference('Workout.count', -1, 'Workout should be destroyed') do
 			delete :destroy, id: @workout
 		end
