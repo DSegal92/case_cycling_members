0f15b4c907879dbafef33cf38265f83d620e090c
Daniel Segal <DSegal92@gmail.com>
2013-12-03 22:56:29 -0500

    minor interface changes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -52,7 +52,7 @@ end
 
     respond_to do |format|
       if @member.save
-        format.html { redirect_to root_url, notice: 'Member was successfully created.' }
+        format.html { redirect_to members_url, notice: 'Member was successfully created.' }
         format.json { render json: @member, status: :created, location: @member }
       else
         format.html { render action: "new" }
