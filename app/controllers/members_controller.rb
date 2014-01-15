ca7d6c241f9eae13898148ba17a855023fd01f4e
Daniel Segal <DSegal92@gmail.com>
2014-01-15 14:30:38 -0500

    Closed un-authorized member editing hole

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -58,7 +58,11 @@ end
 
   # GET /members/1/edit
   def edit
-    @member = Member.find(params[:id])
+    if (current_user && current_user.can_edit)
+      @member = Member.find(params[:id])
+    else
+      redirect_to root_url
+    end
   end
 
   # POST /members
@@ -86,25 +90,29 @@ end
   # PUT /members/1
   # PUT /members/1.json
   def update
-    @member = Member.find(params[:id])
-   
-    respond_to do |format|
-      if @member.update_attributes(params[:member])
-         if (params[:member][:position_ids])
-          @member.is_officer = true
+    if (current_user && current_user.can_edit)
+      @member = Member.find(params[:id])
+    
+      respond_to do |format|
+        if @member.update_attributes(params[:member])
+           if (params[:member][:position_ids])
+            @member.is_officer = true
+          else
+           @member.is_officer = false
+          end
+          if params[:member][:is_verified]
+            @member.is_confirmed = true
+          end
+           @member.save
+          format.html { redirect_to @member, notice: 'Member was successfully updated.' }
+          format.json { head :no_content }
         else
-         @member.is_officer = false
+          format.html { render action: "edit" }
+          format.json { render json: @member.errors, status: :unprocessable_entity }
         end
-        if params[:member][:is_verified]
-          @member.is_confirmed = true
-        end
-         @member.save
-        format.html { redirect_to @member, notice: 'Member was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @member.errors, status: :unprocessable_entity }
       end
+    else
+      redirect_to root_url
     end
   end
 
