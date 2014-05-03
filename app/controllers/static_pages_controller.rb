ca544e034b3b6245c9a0fefa436bd04c21364fe7
Daniel Segal <DSegal92@gmail.com>
2014-05-03 16:50:00 -0400

    Fix error when no secretary found

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,9 @@ class StaticPagesController < ApplicationController
 		@blogs = Blog.find(:all, :order => "id DESC").select { |b| b.internal == true}.first(2)
 		@workout = Workout.select{ |w| w.date >= Time.now}.first()
 		@documents = Document.all
-		@secretary_email = Member.find_by_id(Position.find_by_title("Secretary").member_id).email
+		unless Position.find_by_title("Secretary").null?
+			@secretary_email = Member.find_by_id(Position.find_by_title("Secretary").member_id).email
+		end
 	end
 
 end
